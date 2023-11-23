bd45eead894c82c3492a3d1fa93a5b96c0d53e71
Luciano <luciano01.dev@gmail.com>
2023-11-23 15:48:47 -0300

    Add docs to SignUpRepository.

--- lib/features/domain/repositories/signup/signup_repository.dart
+++ lib/features/domain/repositories/signup/signup_repository.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class SignUpRepository {
+  /// Create an Account by Email and Password, and return a Firebase User.
   Future<User?> createUserWithEmailAndPassword({
     required String email,
     required String password,
