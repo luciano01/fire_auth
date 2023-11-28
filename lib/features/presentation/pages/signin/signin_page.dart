82eab2e77401ede431795985afb54ab75bc64184
Luciano <luciano01.dev@gmail.com>
2023-11-27 21:31:14 -0300

    Initial UI to SignIn.

--- lib/features/presentation/pages/signin/signin_page.dart
+++ lib/features/presentation/pages/signin/signin_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../presentation.dart';
@@ -9,38 +8,107 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final signInStore = Modular.get<SignInStore>();
+    Modular.get<SignInStore>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SignIn'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.login),
-        onPressed: () {
-          signInStore.signInWithEmailAndPassword(
-            email: "jaina@email.com",
-            password: "jaina123",
-          );
-        },
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text("SignUp"),
-              onPressed: () {
-                signInStore.createUserWithEmailAndPassword(
-                  email: "guldanemail.com",
-                  password: "guldan123",
-                );
-              },
-            ),
-            Observer(builder: (_) {
-              return Text(signInStore.errorMessage);
-            }),
-          ],
+      bottomNavigationBar: SizedBox(
+        height: 56,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have an account?"),
+              TextButton(
+                child: const Text("Sign up now!"),
+                onPressed: () => Modular.to.pushNamed("/signup/"),
+              ),
+            ],
+          ),
         ),
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
+        children: [
+          const SizedBox(
+            height: 300,
+            child: FlutterLogo(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+            ).copyWith(top: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                prefixIcon: const Icon(Icons.email_outlined),
+                labelText: "Email",
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              prefixIcon: const Icon(Icons.password_outlined),
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.visibility_off_outlined),
+                onPressed: () {},
+              ),
+              labelText: "Password",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+            ).copyWith(bottom: 32),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all<Size>(
+                  const Size(double.infinity, 56),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              child: const Text("Continue"),
+              onPressed: () {},
+            ),
+          ),
+          OutlinedButton(
+            style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all<Size>(
+                const Size(double.infinity, 56),
+              ),
+              side: MaterialStateProperty.all<BorderSide>(
+                BorderSide(
+                  color: Colors.grey.shade300,
+                ),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlutterLogo(size: 25),
+                SizedBox(width: 10),
+                Text("Sign in with Google"),
+              ],
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
